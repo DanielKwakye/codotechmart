b8c97ec4d2413974416db6741b2f6f36fac909a5
DanielKwakye <30783864+DanielKwakye@users.noreply.github.com>
2018-01-27 01:45:12 +0000

    shopadmin added

---
+++ app/Http/Middleware/RedirectIfShopadmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfShopadmin
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @param  string|null  $guard
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next, $guard = 'shopadmin')
+	{
+	    if (Auth::guard($guard)->check()) {
+	        return redirect('shopadmin/home');
+	    }
+
+	    return $next($request);
+	}
+}
